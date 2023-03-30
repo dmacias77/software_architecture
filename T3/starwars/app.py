5bd9d8b5b91749586b793e070b133d7ae9a28189
Macias <dmacias77@outlook.com>
2023-03-29 19:29:48 -0600

    Corrección de clase

--- T3/starwars/app.py
+++ T3/starwars/app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask
-import requests
+from bl import sw_service
 
 from flask import jsonify
 
@@ -10,15 +10,10 @@ print("PORT::", port)
 
 app = Flask(__name__)
 
-movie_url = "https://swapi.dev/api/films/"
-
-
 @app.route("/", methods=['GET'])
 def list_movies():
-    data = requests.get(movie_url).json()
-    movies = [{"id": movie["episode_id"], "name": movie["title"]} for movie in data["results"]]
+    movies = fetch_movies()
     return jsonify(movies)
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=port)
