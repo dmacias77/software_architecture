9084e0317f360df78003795e1803bb6d705677f2
Macias <dmacias77@outlook.com>
2023-04-03 00:53:27 -0500

    Corrected, 50% working

--- T3/starwars/app.py
+++ T3/starwars/app.py
@@ -1,8 +1,6 @@
 import os
-from bl.sw_service import fetch_movies
-from flask import Flask
-
-from flask import jsonify
+from bl.sw_service import *
+from flask import Flask, jsonify
 
 print("Application startup")
 port = int(os.environ['PORT'])
@@ -10,10 +8,15 @@ print("PORT::", port)
 
 app = Flask(__name__)
 
+url = "https://swapi.dev/api/films/"
+
 @app.route("/", methods=['GET'])
 def list_movies():
-    movies = fetch_movies()
-    return jsonify(movies)
+    return fetchmovies(source=url)
+
+@app.route("/cast", methods=['GET'])
+def list_characters():
+    return fetchcast(source=url)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=port)
--- T3/starwars/bl/sw_service.py
+++ T3/starwars/bl/sw_service.py
@@ -1,21 +1,34 @@
+#starwars_service file
+from flask import jsonify, request
 import requests
 
 dataurl = "https://swapi.dev/api/films/"
 
-def fetch_movies():
-	data = requests.get(dataurl).json()
+def fetchcast(source: str):
+    data = requests.get(source).json()
+    mid = request.args.get('episode_id', default=-1, type=int)
+    cast = []
+    if mid != -1:
+        for i in data["results"]:
+            if i["episode_id"] == mid:
+                cast.extend(i["characters"])
+    else:
+        for i in data["results"]:
+            cast.extend(i["characters"])
+    characters = []
+    for i in cast[0]:
+        data = requests.get(i).json()
+        name = data['name']
+        characters.append(name)
+    return jsonify(characters)
 
-	movies = []
-	for i in data["results"]:
-		charurl = i["characters"]
+def fetchmovies(source: str):
+    data = requests.get(source).json()
 
-		cast = []
-		for j in charurl:
-			data = requests.get(j).json()
-			cast.append(data["name"])
-		movies.append({
-			"id": i["episode_id"],
-			"name": i["title"],
-			"characters": cast
-		})
-	return movies
+    movies_temp = []
+    for i in data["results"]:
+        movie = {"id": i["episode_id"], "name": i["title"]}
+        movies_temp.append(movie)
+    movies = sorted(movies_temp, key=lambda movie: movie['id'])
+
+    return jsonify(movies)
