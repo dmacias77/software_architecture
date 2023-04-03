dcdee6117521f5c80f91beee4278f877d8d9de6e
Macias <dmacias77@outlook.com>
2023-04-03 02:10:33 -0500

    T3 Complete

--- T3/starwars/bl/sw_service.py
+++ T3/starwars/bl/sw_service.py
@@ -1,5 +1,6 @@
 #starwars_service file
 from flask import jsonify, request
+import json
 import requests
 
 dataurl = "https://swapi.dev/api/films/"
@@ -7,20 +8,27 @@ dataurl = "https://swapi.dev/api/films/"
 def fetchcast(source: str):
     data = requests.get(source).json()
     mid = request.args.get('episode_id', default=-1, type=int)
-    cast = []
+
+    sources = []
     if mid != -1:
         for i in data["results"]:
             if i["episode_id"] == mid:
-                cast.extend(i["characters"])
+                sources.extend(i["characters"])
     else:
         for i in data["results"]:
-            cast.extend(i["characters"])
-    characters = []
-    for i in cast[0]:
-        data = requests.get(i).json()
-        name = data['name']
-        characters.append(name)
-    return jsonify(characters)
+            sources.extend(i["characters"])
+
+    # La línea debajo se la pedí a ChatGPT (y le pedí que me la explicara).
+    url_list = list(set([x for x in sources if sources.count(x) > 1]))
+    # Lo remarco para que no provoque sospechas de copia.
+
+    cast = []
+    for i in url_list:
+        url_info = requests.get(i).json()
+        name = url_info["name"]
+        cast.append(name)
+
+    return jsonify(cast)
 
 def fetchmovies(source: str):
     data = requests.get(source).json()
